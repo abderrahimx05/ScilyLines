21250f523429a84295f1859b7d9a0ad6b08c7d68
abderrahimx05 <75130312+abderrahimx05@users.noreply.github.com>
2022-10-05 23:29:27 +0200

    fix the problem

--- ScilyLines/DAL/LaissonDAO.cs
+++ ScilyLines/DAL/LaissonDAO.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using ScilyLines.Modele;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,83 @@ using System.Threading.Tasks;
 
 namespace ScilyLines.DAL
 {
-    internal class Laisson
+    public class Laisson
     {
-        
-       
+        private static string provider = "localhost";
+
+        private static string dataBase = "scilyline";
+
+        private static string uid = "scilyline";
+
+        private static string mdp = "AbderBen22";
+
+
+
+        private static Connexion maConnexionSql;
+
+
+        private static MySqlCommand com;
+        public static List<Secteur> getSecteur()
+        {
+
+            List<Secteur> lc = new List<Secteur>();
+
+            try
+            {
+
+                maConnexionSql = Connexion.getInstance(provider, dataBase, uid, mdp);
+
+
+                maConnexionSql.openConnection();
+
+
+                com = maConnexionSql.reqExec("Select * from secteur");
+
+
+                MySqlDataReader reader = com.ExecuteReader();
+
+                Secteur e;
+
+                while (reader.Read())
+                {
+
+
+                    string nom = (string)reader.GetValue(1);
+
+
+
+                    e = new Secteur(nom);
+
+
+                    lc.Add(e);
+
+
+                }
+
+
+
+
+
+
+                reader.Close();
+
+                maConnexionSql.closeConnection();
+
+
+                return (lc);
+
+
+            }
+
+            catch (Exception emp)
+            {
+
+                throw (emp);
+
+            }
+
+        }
+
+
     }
 }
